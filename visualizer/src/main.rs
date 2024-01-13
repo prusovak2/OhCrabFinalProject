96bc79d65b1298a8b5b88c73a840cc21464bad5e
prusovak <katerina.prusova@quanti.cz>
2024-01-13 18:01:52 +0100

    try audio tool

--- visualizer/src/main.rs
+++ visualizer/src/main.rs
@@ -1,9 +1,12 @@
 use oxagworldgenerator::world_generator::{OxAgWorldGenerator, world_generator_builder::OxAgWorldGeneratorBuilder};
-use robotics_lib::world::world_generator::Generator;
+use visualizer::{visualizer::try_ggeui, audio::try_sound};
+// use robotics_lib::world::world_generator::Generator;
 use visualizer::{mock::*, history_cache::try_cache};
 
 fn main() {
     println!("Hello, world!");
+    //try_sound();
+    //try_ggeui();
     test_runner_main_loop();
     //try_cache()
 }
